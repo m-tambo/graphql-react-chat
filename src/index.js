4126f95707b2fd633a6ca30e691af77ff5e6edc5
Michael Tambornino <michael.tambornino@gmail.com>
2018-03-11 18:21:23 -0500

    Wrap App in Apollo client

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index.css'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-client-preset'
+import SERVICE_ID from './.env'
+
+const httpLink = new HttpLink({ uri: `https://api.graph.cool/simple/v1/${SERVICE_ID}` })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
